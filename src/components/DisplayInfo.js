06f284e567924fe7bad5b0e405f955b284ae52c9
Alina <alina.dolgoy@gmail.com>
2019-02-11 16:31:18 -0500

    added key to map and fixed error display issues

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,21 +1,24 @@
 import React from 'react';
+import Error from './ErrorMessage.js'
 
-const  DisplayInfo = ({repos = [], pullRequests = []}) => {
-    
+const  DisplayInfo = ({ repos = [], pullRequests = [] , noPrError='', noRepoError=''}) => {
+
     return (
     <div>
         <h2>Forked Repositories</h2>
-            <ul>
-                {repos.map(repo => 
-                    <li>
+        <Error error={noRepoError} />
+        <ul>
+                {repos.map((repo,i) => 
+                    <li key={i}>
                         <a href={`https://github.com/${repo.repoLink}`}> {repo.repoName} </a> <br />
                         <a href={`https://github.com/${repo.baseRepo}`}>Base repo</a>
                     </li>)}
             </ul>
         <h2>Pull Requests</h2>    
+        <Error error={noPrError} />
             <ul>
-                {pullRequests.map(pullRequest => 
-                    <li>
+                {pullRequests.map((pullRequest,i) => 
+                    <li key={i}>
                         <a href={`${pullRequest.PrLink}`}>{pullRequest.PrTitle}</a>    
                         {pullRequest.status}
                         {pullRequest.merged}
