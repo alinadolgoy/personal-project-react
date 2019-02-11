c35b6ad34d4b5c9cb88bc9fe3874acbdde6b7578
Alina <alina.dolgoy@gmail.com>
2019-02-11 14:30:50 -0500

    added the user name to the fetch query and fixed issues with the display if statement

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
   // this actiob will fire once the user click the submit button. It will take the user name that was entered call the fetch and transformation functions. 
   retreiveTransformAndSaveData() {
     this.setState({submitClicked: true});
-    getInfoFromApi()
+    getInfoFromApi(this.state.userName)
     .then(transformedData => this.setState({payloads: transformedData}))
     .catch((error) => this.setState({errorMessage: error})) 
   }
@@ -43,13 +43,23 @@ class App extends Component {
     //if username is empty AND repos is empty (this will the check for when to display error and when to display info )
       const displayOptions = () => {
         let result;
-        if (this.state.submitClicked && this.state.payloads.repos.length>0 ) {
+        // if user has both forked repos and pull requests
+        if (this.state.submitClicked && this.state.payloads.repos.length>0 && this.state.payloads.pullRequests.length>0) {
           result = <DisplayInfo repos={this.state.payloads.repos} pullRequests={this.state.payloads.pullRequests} />
         }
-        else if (this.state.submitClicked && this.state.payloads === 0){
-        // add the prs vs repos here
-          result = <Error error="This user has no data" />
+        // if user has repos but not pull requests
+        else if (this.state.submitClicked && this.state.payloads.repos.length > 0 && this.state.payloads.pullRequests.length === 0 ){
+          result = <div><DisplayInfo repos={this.state.payloads.repos} /> <Error error="This user has no recent pull requests" /></div>
         }
+        // if user has pull requests but not repos
+        else if (this.state.submitClicked && this.state.payloads.repos.length === 0 && this.state.payloads.pullRequests.length > 0) {
+          result = <div><DisplayInfo pullRequests={this.state.payloads.pullRequests} /> <Error error="This user has no recent forked repos" /></div>
+        }
+        //if user has no forked repos and no pull reuqests
+        else if (this.state.submitClicked && this.state.payloads.repos.length === 0 && this.state.payloads.pullRequests.length === 0) {
+          result = <Error error="This user has no recent forked repos and no recent pull requests" />
+        }
+        // if userName has not been submitted yet 
         else {
           result =  <Error error="please enter user name and click submit" />
         }
@@ -71,3 +81,4 @@ class App extends Component {
 
   
 export default App;
+
